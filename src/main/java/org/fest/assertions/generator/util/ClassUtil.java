47826d4be2da0fa74afc812be27c4250195cb5bf
Joel Costigliola <joel.costigliola@gmail.com>
2013-03-02 15:40:20 +0100

    github #27 - CLI fails to generate assertions when specifying a package whose classes are in a jar

--- src/main/java/org/fest/assertions/generator/util/ClassUtil.java
+++ src/main/java/org/fest/assertions/generator/util/ClassUtil.java
@@ -16,11 +16,18 @@ import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 /**
  * 
@@ -77,15 +84,36 @@ public class ClassUtil {
    * @return the list of Class found
    * @throws RuntimeException if any error occurs
    */
-  private static List<Class<?>> getClassesInPackage(String packageName, ClassLoader classLoader) {
+  private static Set<Class<?>> getClassesInPackage(String packageName, ClassLoader classLoader) {
+    if (classLoader == null) {
+      throw new IllegalArgumentException("Null class loader.");
+    }
+    // load classes from classpath file system, this won't load classes in jars
+    Set<Class<?>> packageClasses = getPackageClassesFromClasspathFiles(packageName, classLoader);
+    // load classes from classpath file system, this won't load classes in jars
+    Set<Class<?>> packageClassesFromClasspathJars = getPackageClassesFromClasspathJars(packageName, classLoader);
+    packageClasses.addAll(packageClassesFromClasspathJars);
+    return packageClasses;
+  }
+
+  private static Set<Class<?>> getPackageClassesFromClasspathJars(String packageName, ClassLoader classLoader) {
+    List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+    classLoadersList.add(ClasspathHelper.contextClassLoader());
+    classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+        .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+        .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
+    return reflections.getSubTypesOf(Object.class);
+  }
+
+  private static Set<Class<?>> getPackageClassesFromClasspathFiles(String packageName, ClassLoader classLoader) {
     try {
-      if (classLoader == null) {
-        throw new IllegalArgumentException("Null class loader.");
-      }
       String packagePath = packageName.replace('.', File.separatorChar);
       // Ask for all resources for the path
       Enumeration<URL> resources = classLoader.getResources(packagePath);
-      List<Class<?>> classes = new ArrayList<Class<?>>();
+      Set<Class<?>> classes = new HashSet<Class<?>>();
       while (resources.hasMoreElements()) {
         File directory = new File(URLDecoder.decode(resources.nextElement().getPath(), "UTF-8"));
         if (directory.canRead()) {

16c73f29ed215e2ef608920615129fc99666881d
Joel Costigliola <joel.costigliola@gmail.com>
2015-07-25 23:42:42 +1200

    Use latest parent pom. Stick to JUnit 4.11 as 4.12 version breaks Theories defined in superclass. Update javadoc class licence yearcomment

--- src/test/java/org/assertj/assertions/generator/NestedClassesTest.java
+++ src/test/java/org/assertj/assertions/generator/NestedClassesTest.java
@@ -8,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  */
 package org.assertj.assertions.generator;
 
@@ -20,36 +20,40 @@ import org.junit.experimental.theories.DataPoint;
  * This class contains a set of constants for nested classes.
  */
 public interface NestedClassesTest {
-    @DataPoint
-    public static final NestedClass SNC = new NestedClass(OuterClass.StaticNestedPerson.class, "OuterClass.StaticNestedPerson");
-    @DataPoint
-    public static final NestedClass SNC_SNC = new NestedClass(OuterClass.StaticNestedPerson.SNP_StaticNestedPerson.class, "OuterClass.StaticNestedPerson.SNP_StaticNestedPerson");
-    @DataPoint
-    public static final NestedClass SNC_IC = new NestedClass(OuterClass.StaticNestedPerson.SNP_InnerPerson.class, "OuterClass.StaticNestedPerson.SNP_InnerPerson");
-    @DataPoint
-    public static final NestedClass IC = new NestedClass(OuterClass.InnerPerson.class, "OuterClass.InnerPerson");
-    @DataPoint
-    public static final NestedClass IC_IC = new NestedClass(OuterClass.InnerPerson.IP_InnerPerson.class, "OuterClass.InnerPerson.IP_InnerPerson");
+  @DataPoint
+  public static final NestedClass SNC = new NestedClass(OuterClass.StaticNestedPerson.class,
+                                                        "OuterClass.StaticNestedPerson");
+  @DataPoint
+  public static final NestedClass SNC_SNC = new NestedClass(OuterClass.StaticNestedPerson.SNP_StaticNestedPerson.class,
+                                                            "OuterClass.StaticNestedPerson.SNP_StaticNestedPerson");
+  @DataPoint
+  public static final NestedClass SNC_IC = new NestedClass(OuterClass.StaticNestedPerson.SNP_InnerPerson.class,
+                                                           "OuterClass.StaticNestedPerson.SNP_InnerPerson");
+  @DataPoint
+  public static final NestedClass IC = new NestedClass(OuterClass.InnerPerson.class, "OuterClass.InnerPerson");
+  @DataPoint
+  public static final NestedClass IC_IC = new NestedClass(OuterClass.InnerPerson.IP_InnerPerson.class,
+                                                          "OuterClass.InnerPerson.IP_InnerPerson");
 
-    public static class NestedClass {
-        private final Class<?> nestedClass;
-        private final String classNameWithOuterClass;
+  public static class NestedClass {
+    private final Class<?> nestedClass;
+    private final String classNameWithOuterClass;
 
-        public NestedClass(Class<?> nestedClass, String classNameWithOuterClass) {
-            this.nestedClass = nestedClass;
-            this.classNameWithOuterClass = classNameWithOuterClass;
-        }
-
-        public String getClassNameWithOuterClass() {
-            return classNameWithOuterClass;
-        }
-
-        public String getClassNameWithOuterClassNotSeparatedBytDots() {
-          return StringUtils.remove(classNameWithOuterClass, '.');
-        }
-        
-        public Class<?> getNestedClass() {
-            return nestedClass;
-        }
+    public NestedClass(Class<?> nestedClass, String classNameWithOuterClass) {
+      this.nestedClass = nestedClass;
+      this.classNameWithOuterClass = classNameWithOuterClass;
     }
+
+    public String getClassNameWithOuterClass() {
+      return classNameWithOuterClass;
+    }
+
+    public String getClassNameWithOuterClassNotSeparatedBytDots() {
+      return StringUtils.remove(classNameWithOuterClass, '.');
+    }
+
+    public Class<?> getNestedClass() {
+      return nestedClass;
+    }
+  }
 }
